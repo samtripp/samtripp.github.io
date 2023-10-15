bab982977dd260a5aa2fc0689a35107920ae237a
samtripp <sam@samtripp.com>
2023-10-15 13:12:36 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+/** npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch **/
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
