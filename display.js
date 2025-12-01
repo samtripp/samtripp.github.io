e01ba3139139967a51087fa410a6d022f13d0520
samtripp <sam@samtripp.com>
2025-12-01 10:55:33 +0000

    Flipdot tests

---
+++ display.js
@@ -0,0 +1,133 @@
+// display_webserial.js
+// Browser-compatible ES Module using Web Serial API
+
+const HEADER = '\x02';
+const FOOTER = '\x03';
+
+/**
+ * Converts a hex character to 4-bit binary string
+ */
+function chrToHexTo4bit(character) {
+  const number = parseInt(character, 16);
+  return number.toString(2).padStart(4, '0');
+}
+
+/**
+ * Decode 2 hex characters to a number
+ */
+function decode2BytesToNumber(one, two) {
+  const bits = chrToHexTo4bit(one) + chrToHexTo4bit(two);
+  return parseInt(bits, 2);
+}
+
+/**
+ * Encode number to 2 hex characters
+ */
+function encodeNumberTo2Bytes(number) {
+  const binary = number.toString(2).padStart(8, '0');
+  const one = parseInt(binary.slice(0, 4), 2).toString(16).toUpperCase();
+  const two = parseInt(binary.slice(4), 2).toString(16).toUpperCase();
+  return [one, two];
+}
+
+/**
+ * Calculate checksum for data
+ */
+function calculateChecksum(data, header, head) {
+  let summed = 0;
+  const tmp = [head.charCodeAt(0), ...header.map(c => c.charCodeAt(0))];
+  summed += tmp.reduce((a, b) => a + b, 0);
+
+  const dataBytes = [];
+  for (const number of data) {
+    for (const val of encodeNumberTo2Bytes(number)) {
+      dataBytes.push(val.charCodeAt(0));
+    }
+  }
+
+  summed += dataBytes.reduce((a, b) => a + b, 0);
+  summed += 1;
+
+  summed &= 0xFF;
+  summed ^= 255;
+  summed += 1;
+  return summed;
+}
+
+/**
+ * Encode a matrix string (with "#" and "-") into a byte string
+ */
+function encode(matrixHashDash, address = 1) {
+  const matrix = matrixHashDash.trim().split('\n');
+  const constant = 1;
+  const rows = matrix.length;
+  const columns = matrix[0].length;
+
+  const header = [String(constant), String(address)];
+  const dataSize = (rows * columns) / 8;
+  for (const c of encodeNumberTo2Bytes(dataSize)) header.push(c);
+
+  const data = [];
+  for (let col = 0; col < columns; col++) {
+    const column = matrix.map(row => row[col]).join('').replace(/[- ]/g, '0').replace(/#/g, '1');
+    const reversed = column.split('').reverse().join('');
+    const nibbles = [
+      reversed.slice(8, 12),
+      reversed.slice(12, 16),
+      reversed.slice(0, 4),
+      reversed.slice(4, 8),
+    ];
+    for (const nib of nibbles) {
+      const num = parseInt(nib, 2);
+      data.push(num.toString(16).toUpperCase());
+    }
+  }
+
+  const checksumData = [];
+  for (let i = 0; i < data.length; i += 2) {
+    checksumData.push(decode2BytesToNumber(data[i], data[i + 1]));
+  }
+
+  let footer = calculateChecksum(checksumData, header, HEADER);
+  footer = encodeNumberTo2Bytes(footer);
+
+  return [HEADER, ...header, ...data, FOOTER, ...footer].join('');
+}
+
+// Web Serial API
+let port;
+let writer;
+
+/**
+ * Request access to a serial port
+ */
+async function requestPort() {
+  port = await navigator.serial.requestPort();
+  await port.open({ baudRate: 4800 });
+  writer = port.writable.getWriter();
+}
+
+/**
+ * Send encoded data to the serial port
+ */
+async function writeIt(encoded) {
+  if (!writer) throw new Error("Serial port not open. Call requestPort() first.");
+  const encoder = new TextEncoder();
+  await writer.write(encoder.encode(encoded));
+}
+
+/**
+ * Display matrix on the serial device
+ * @param {Array<Array<number>>} matrix
+ * @param {number} address
+ */
+async function displayMain(matrix, address = 1) {
+  const matrixStr = matrix.map(row => row.join('').replace(/0/g, '-').replace(/1/g, '#')).join('\n');
+  const encoded = encode(matrixStr, address);
+  if (!port || !writer) {
+    await requestPort();
+  }
+  await writeIt(encoded);
+}
+
+export { displayMain, requestPort, writeIt, encode };
